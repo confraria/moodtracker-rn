209f1c4a0953b99b376720bb8aeab7c34c801a63
Luis Confraria <lconfraria@gmail.com>
2022-09-28 18:15:15 +0100

    fix swipe to delete in android

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {AppProvider} from './App.provider';
 import {BottomTabsNavigator} from './screens/BottomTabs.navigator';
 
 export const App: React.FC = () => {
   return (
-    <AppProvider>
-      <NavigationContainer>
-        <BottomTabsNavigator />
-      </NavigationContainer>
-    </AppProvider>
+    <GestureHandlerRootView style={{flex: 1}}>
+      <AppProvider>
+        <NavigationContainer>
+          <BottomTabsNavigator />
+        </NavigationContainer>
+      </AppProvider>
+    </GestureHandlerRootView>
   );
 };
